ddf8a5f8b0c6b0f187221ee7f113d1069ff7e8fe
Tracy Chen <stchen1012@gmail.com>
2019-10-23 15:57:16 -0400

    added comment controller

--- src/main/java/com/ga/controller/CommentController.java
+++ src/main/java/com/ga/controller/CommentController.java
@@ -1,5 +1,23 @@
 package com.ga.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ga.service.CommentService;
+import com.ga.service.PostService;
+
+@RestController
+@RequestMapping("/comment")
 public class CommentController {
 
+	@Autowired
+	CommentService commentService;
+	
+	@DeleteMapping("/{commentId}")
+	public Long deleteComment(@PathVariable Long commentId) {
+		return commentService.deleteComment(commentId);
+	}
 }
