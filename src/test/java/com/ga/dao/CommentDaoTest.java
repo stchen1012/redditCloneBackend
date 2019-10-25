eeaaf6c817ffd9be403b5cbec8243634e7d78528
Phil Lee <phil.j.lee1@gmail.com>
2019-10-25 14:49:21 -0400

    login signup test

--- src/test/java/com/ga/dao/CommentDaoTest.java
+++ src/test/java/com/ga/dao/CommentDaoTest.java
@@ -1,5 +1,90 @@
 package com.ga.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import com.ga.entity.Comment;
+import com.ga.entity.Post;
+import com.ga.entity.User;
+
 public class CommentDaoTest {
+	
+	private List<Comment> comments;
+	private Comment comment;
+	
+	@InjectMocks
+    private User user;
+
+    @InjectMocks
+    CommentDaoImpl commentDao;
+    
+    @Mock
+    private SessionFactory sessionFactory;
+    
+    @Mock
+    Query<User> query;
+
+    @Mock
+    Query<Comment> queryComment;
+    
+    @Mock
+    Session session;
+    
+    @Mock
+    Transaction transaction;
+    
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+    
+    @Before
+    public void initializer() {
+    	comment = new Comment();
+    	comments = new ArrayList<Comment>();
+        
+    	comment.setCommentId(1L);
+    	comment.setText("some post");
+    	comments.add(comment);
+    	
+    	user.setUserId(1L);
+        user.setUsername("testuser");
+        user.setPassword("testpass");
+
+        when(sessionFactory.getCurrentSession()).thenReturn(session);
+        when(session.getTransaction()).thenReturn(transaction);
+    }
+	
+    @Test
+    public void deleteComment_Long_SUCCESS() {
+    	when(session.createQuery(anyString())).thenReturn(query, queryComment);
+    	when(query.uniqueResult()).thenReturn(user);
+    	when(queryComment.getResultList()).thenReturn(comments);
+    	
+    	List<Comment> commentByUser = commentDao.getAllCommentByUserName("someUser");
+    	
+    	assertNotNull("Tested returned null obj, expected not null",comment);
+    	assertEquals(comment.getText(), commentByUser.get(0).getText());
+    }
+    
+    @Test
+    public void getAllCommentByUserName_Posts_SUCCESS() {
+    	
+    }
 
 }
--- src/test/java/com/ga/service/UserServiceTest.java
+++ src/test/java/com/ga/service/UserServiceTest.java
@@ -1,5 +1,80 @@
 package com.ga.service;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.ga.config.JwtUtil;
+import com.ga.dao.UserDao;
+import com.ga.entity.User;
+import com.ga.entity.UserProfile;
+
 public class UserServiceTest {
 
+	@Mock
+	UserDao userDao;
+	
+	@Mock
+	UserProfile userProfile;
+	
+	@Mock
+	private JwtUtil jwtUtil;
+
+	@Mock
+	private PasswordEncoder bCryptPasswordEncoder;
+
+	@InjectMocks
+	private User user;
+
+	@InjectMocks
+	private UserServiceImpl userService;
+	
+	@Before
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
+		user.setUserId(1L);
+		user.setPassword("123");
+		user.setUsername("batman");
+		
+		userProfile.setAddress("124");
+		userProfile.setEmail("aa@aa.com");
+		userProfile.setMobile("333-333-3333");
+		user.setUserProfile(userProfile);		
+	}
+
+	@Test
+	public void signup_String_SUCCESS() {
+		String expectedToken = "12345";
+
+		when(userDao.signup(any())).thenReturn(user);
+		when(userDao.getUserByUsername(anyString())).thenReturn(user);
+		when(jwtUtil.generateToken(any())).thenReturn(expectedToken);
+		when(bCryptPasswordEncoder.encode(any())).thenReturn("robin");
+		System.out.println(user.getUsername());
+		String actualToken = userService.signup(user);
+
+		assertEquals(actualToken, expectedToken);
+	}
+	
+	@Test
+    public void login_User_Success() {
+        String expectedToken = "12345";
+        
+        when(userDao.login(any())).thenReturn(user);
+        when(bCryptPasswordEncoder.matches(any(), any())).thenReturn(true);
+        when(userDao.getUserByUsername(anyString())).thenReturn(user);
+        when(jwtUtil.generateToken(any())).thenReturn(expectedToken);
+        when(bCryptPasswordEncoder.encode(user.getPassword())).thenReturn("robin");
+        
+        String actualToken = userService.login(user);
+        assertEquals(actualToken, expectedToken);
+        
+    }
 }
